d2d7edeed11b34d7f26392a65d0676e386455383
iirthw <iirthw@gmail.com>
2021-12-07 23:46:38 +0200

    [cmake] wip on structure

--- test/main.cpp
+++ test/main.cpp
@@ -1,13 +1,7 @@
 #include "gtest/gtest.h"
 
-TEST(FooTest, barTest)
-{
-    ASSERT_EQ(0, 0);
-}
-
-int main(int argc, char **argv)
+int main(int argc, char* argv[])
 {
     ::testing::InitGoogleTest(&argc, argv);
-
     return RUN_ALL_TESTS();
 }
