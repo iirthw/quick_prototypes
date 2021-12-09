d53b4ec20ce9cddd12b922bd32ef87dac1f4a41c
iirthw <iirthw@gmail.com>
2021-12-09 23:46:19 +0200

    [test] add test_rectangle

--- test/test_rectangle.cpp
+++ test/test_rectangle.cpp
@@ -1,12 +1,13 @@
 #include "gtest/gtest.h"
 
 #include "Rectangle.h"
+#include "util/Utils.h"
 
 using namespace util;
 
 TEST(RectangleTest, noargCtor)
 {
-	geom::Rectangle rect;
+	geom::Rect rect;
 
 	ASSERT_EQ(rect.x, 0.0f);
 	ASSERT_EQ(rect.y, 0.0f);
@@ -17,17 +18,17 @@ TEST(RectangleTest, noargCtor)
 TEST(RectangleTest, area)
 {
 	{
-		geom::Rectangle rect;
+		geom::Rect rect;
     	EXPECT_TRUE(isZero(rect.area()));
     }
 
     {
-    	geom::Rectangle rect(0.0f, 0.0f, 1.0f, 10.0f);
+    	geom::Rect rect(0.0f, 0.0f, 1.0f, 10.0f);
     	EXPECT_TRUE(isZero(rect.area() - 10.0f));	
     }
 
     {
-    	geom::Rectangle rect(10.0f, 10.0f, 5.0f, 5.0f);
+    	geom::Rect rect(10.0f, 10.0f, 5.0f, 5.0f);
     	EXPECT_TRUE(isZero(rect.area() - 25.0f));	
     }
 }
