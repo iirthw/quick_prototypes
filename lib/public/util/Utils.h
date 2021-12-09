4deb8287794a8edf6ff074a6f3d86969e5d8d23b
iirthw <iirthw@gmail.com>
2021-12-09 23:45:16 +0200

    [utils] fix compilation

--- lib/public/util/Utils.h
+++ lib/public/util/Utils.h
@@ -4,16 +4,16 @@
 
 namespace util
 {
+    inline bool isEqual(double x, double y)
+    {
+        // See https://isocpp.org/wiki/faq/newbie#floating-point-arith
+        // and also Knuth section 4.2.2 pages 217-218
+          const double epsilon = 1e-8;
+          return std::abs(x - y) <= epsilon * std::abs(x);
+    }
+
 	inline bool isZero(double x)
 	{
 		return isEqual(x, 0.0);
 	}
-
-	inline bool isEqual(double x, double y)
-	{
-		// See https://isocpp.org/wiki/faq/newbie#floating-point-arith
-		// and also Knuth section 4.2.2 pages 217-218
-	  	const double epsilon = 1e-8;
-	  	return std::abs(x - y) <= epsilon * std::abs(x);
-	}
-}
+} // namespace util
