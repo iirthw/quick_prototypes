07593c33fadcabbcf2f3d408b3dc98ea7c8410dc
iirthw <iirthw@gmail.com>
2021-12-10 23:57:53 +0200

    [lib] wip on Image

--- lib/public/Image.h
+++ lib/public/Image.h
@@ -1,9 +1,47 @@
 #pragma once
 
+#include "core/vec2.h"
+
 namespace image
 {
-	class Image
-	{
-		// TODO: impl
+    class GrayscaleImage
+    {
+    public:
+        GrayscaleImage(unsigned char* image, int width, int height);
+        GrayscaleImage(int width, int height);
+        ~GrayscaleImage();
+        GrayscaleImage(const GrayscaleImage& other);
+        GrayscaleImage& operator= (const GrayscaleImage&) = delete;
+        GrayscaleImage(GrayscaleImage&&) = delete;
+        GrayscaleImage& operator= (GrayscaleImage&&) = delete;
+      
+      /**
+       * Get value of the image pixel.
+       */
+      unsigned char getPixel(int i, int j) const;
+
+      /**
+       * Set image pixel value.
+       */
+      void setPixel(int i, int j, unsigned char value);
+
+      /**
+       * Get image center.
+       */
+      constexpr vec2 center() const;
+      
+      /**
+       * Get raw image buffer data.
+       */
+      inline unsigned char* data() const { return mImg; }
+
+      inline int width() const { return mWidth; }
+      inline int height() const { return mHeight; }
+      
+    private:
+      
+      int mWidth;
+      int mHeight;
+      unsigned char* mData;
 	}; // class Image
 }; // namespace image
